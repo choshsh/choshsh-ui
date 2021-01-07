73b7b8737265f3a5475b51fdc2db204e5a01b53a
SHsteak <cho911115@gmail.com>
2021-01-07 22:28:54 +0900

    refresh

--- src/main/java/com/itsmv/api/calendar/CalendarRepo.java
+++
@@ -1,9 +0,0 @@
-package com.itsmv.api.calendar;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CalendarRepo extends CrudRepository<CalendarEntity, String> {
-
-}
--- src/main/java/com/itsmv/api/menu/HeaderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.itsmv.api.menu;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface HeaderRepo extends CrudRepository<HeaderEntity, Long> {
-
-}
