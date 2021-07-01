e41f9f0565a348383676c44f1c4c6903cc53f204
Sang-hyun, Cho <cho911115@gmail.com>
2021-07-01 14:41:33 +0900

    edit test config

--- src/test/java/com/itsmv/ItsmvApplicationTests.java
+++ src/test/java/com/itsmv/ItsmvApplicationTests.java
@@ -2,12 +2,14 @@ package com.itsmv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ItsmvApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
