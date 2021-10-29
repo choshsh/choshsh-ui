771fc305d79d36362f16416bf63c14257e8ae365
Sang-hyun, Cho <cho911115@gmail.com>
2021-10-29 20:30:23 +0900

    API Doc 추가

--- src/main/java/com/choshsh/choshshui/api/prometheus/PrometheusController.java
+++ src/main/java/com/choshsh/choshshui/api/prometheus/PrometheusController.java
@@ -1,5 +1,6 @@
 package com.choshsh.choshshui.api.prometheus;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,6 +17,7 @@ public class PrometheusController {
     this.webClient = webClient;
   }
 
+  @ApiOperation(value = "Prometheus 알림 규칙 조회")
   @GetMapping(PREFIX_URL + "/rules")
   public Mono<PrometheusDTO> getRules() {
     return webClient.get()
@@ -24,6 +26,7 @@ public class PrometheusController {
         .bodyToMono(PrometheusDTO.class);
   }
 
+  @ApiOperation(value = "Prometheus 활성 알림 조회")
   @GetMapping(PREFIX_URL + "/alerts")
   public Mono<PrometheusDTO> getAlerts() {
     return webClient.get()
