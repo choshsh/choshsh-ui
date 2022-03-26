c916dcfe14c1e1e0fd9738d66f5f72451abf01f9
Sang-hyun, Cho <cho911115@gmail.com>
2022-03-27 00:11:22 +0900

    grafana 실행 스크립트 추가 (docker-compose)
    - dashboard 자동 import
    - iframe 설정 완료

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,13 +8,13 @@ VALUES
 
 INSERT INTO iframe (category, url, name, height, tooltip, sort)
 VALUES
-     ('log', 'http://grafana.choshsh.com/d/XeS0yvN7z/kube-all-resource-log?orgId=1', 'ALL', '1100', null,1),
-     ('log', 'http://grafana.choshsh.com/d/JqglEFN7z/kube-control-plane-log?orgId=1', 'Control Plane', '1000', 'API서버, 컨트롤러 매니저, 스케줄러, etcd, CoreDNS, kube-proxy', 2),
-     ('metric', 'http://grafana.choshsh.com/d/4b545447f/kubernetes-all-in-one-cluster-monitoring-kr?orgId=1', 'Kubernetes', '1800', null, 1),
-     ('metric', 'http://grafana.choshsh.com/d/LJ_uJAvmk/istio-service-dashboard?orgId=1', 'Istio', '2300', 'Istio 네트워크 모니터링', 2),
-     ('metric', 'http://grafana.choshsh.com/d/ku9BHmOnz/jvm-micrometer?orgId=1', 'JVM', '3300', 'Prometheus Kubernetes SD를 위한 Annotation이 있는 JVM 앱 모니터링', 3),
-     ('loadtest', 'http://grafana.choshsh.com/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'from', '1200', null, 1),
-     ('loadtest', 'http://grafana.choshsh.com/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'to', '1200', null, 2);
+     ('log', 'http://localhost:3000/d/XeS0yvN7z/kube-all-resource-log?orgId=1', 'ALL', '1100', null,1),
+     ('log', 'http://localhost:3000/d/JqglEFN7z/kube-control-plane-log?orgId=1', 'Control Plane', '1000', 'API서버, 컨트롤러 매니저, 스케줄러, etcd, CoreDNS, kube-proxy', 2),
+     ('metric', 'http://localhost:3000/d/4b5r45447f/kubernetes-all-in-one-cluster-monitoring-kr?orgId=1', 'Kubernetes', '1800', null, 1),
+     ('metric', 'http://localhost:3000/d/LJ_uJAvmk/istio-service-dashboard?orgId=1', 'Istio', '2300', 'Istio 네트워크 모니터링', 2),
+     ('metric', 'http://localhost:3000/d/ku9BHmOnz/jvm-micrometer?orgId=1', 'JVM', '3300', 'Prometheus Kubernetes SD를 위한 Annotation이 있는 JVM 앱 모니터링', 3),
+     ('loadtest', 'http://localhost:3000/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'from', '1200', null, 1),
+     ('loadtest', 'http://localhost:3000/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'to', '1200', null, 2);
 
 INSERT INTO user (user_id, user_name, user_pw, role)
 VALUES
