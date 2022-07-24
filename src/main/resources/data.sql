2855bd1029c2322f1a394dd10d2024b920d49517
Sang-hyun, Cho <cho911115@gmail.com>
2022-07-24 19:48:16 +0900

    featrue: Add prometheus config for local dev (docker-compose) (#120)
    * featrue: Add prometheus config for local dev (docker-compose)
    
    * ci: Generate springdoc and html file automatically

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,15 +4,15 @@ VALUES
      ('https://www.notion.so/choshsh/9019293e1d86475fb19fe0a5d1149f0d', 'SH-Notion', 2, 'Y'),
      ('http://jenkins.choshsh.com', 'Jenkins', 3, 'Y'),
      ('http://jaeger.choshsh.com', 'Jaeger', 4, 'Y'),
-     ('/settings', '설정', 99, 'N');
+     ('/settings', 'Settings', 99, 'N');
 
 INSERT INTO iframe (category, url, name, height, tooltip, sort)
 VALUES
      ('log', 'http://localhost:3000/d/XeS0yvN7z/kube-all-resource-log?orgId=1', 'ALL', '1100', null,1),
      ('log', 'http://localhost:3000/d/JqglEFN7z/kube-control-plane-log?orgId=1', 'Control Plane', '1000', 'API서버, 컨트롤러 매니저, 스케줄러, etcd, CoreDNS, kube-proxy', 2),
-     ('metric', 'http://localhost:3000/d/4b545447f/kubernetes-all-in-one-cluster-monitoring-kr?orgId=1', 'Kubernetes', '1800', null, 1),
-     ('metric', 'http://localhost:3000/d/LJ_uJAvmk/istio-service-dashboard?orgId=1', 'Istio', '2300', 'Istio 네트워크 모니터링', 2),
-     ('metric', 'http://localhost:3000/d/ku9BHmOnz/jvm-micrometer?orgId=1', 'JVM', '3300', 'Prometheus Kubernetes SD를 위한 Annotation이 있는 JVM 앱 모니터링', 3),
+     ('metric', 'http://localhost:3000/d/ku9BHmOnz/jvm-micrometer?orgId=1', 'JVM', '3300', 'JVM monitoring', 1),
+     ('metric', 'http://localhost:3000/d/4b545447f/kubernetes-all-in-one-cluster-monitoring-kr?orgId=1', 'Kubernetes', '1800', 'k8s monitoring', 2),
+     ('metric', 'http://localhost:3000/d/LJ_uJAvmk/istio-service-dashboard?orgId=1', 'Istio', '2300', 'Istio monitoring', 3),
      ('loadtest', 'http://localhost:3000/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'from', '1200', null, 1),
      ('loadtest', 'http://localhost:3000/d/U8-JyvH7k/choshsh-pod-metric?orgId=1', 'to', '1200', null, 2);
 
