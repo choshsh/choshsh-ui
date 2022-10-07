49861506fb1c50fd6b0210fbde0788caaf9d71e0
Sang-hyun, Cho <cho911115@gmail.com>
2022-10-07 13:26:28 +0900

    update

--- src/main/java/com/choshsh/choshshui/ChoshshUiApplication.java
+++ src/main/java/com/choshsh/choshshui/ChoshshUiApplication.java
@@ -2,10 +2,12 @@ package com.choshsh.choshshui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
+@SpringBootApplication
 public class ChoshshUiApplication {
 
   public static void main(String[] args) {
